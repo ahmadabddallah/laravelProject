88ab718b620cdd0646e68e8e8707988e9df62ebc
Ahmed Abdallah <ahmadabddallah@gmail.com>
2025-08-25 19:49:13 +0300

    first commit

---
+++ blog/app/Http/Controllers/PostsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    //
+    public function index(){
+        $allPosts = [
+            ['id' => 1, 'title' => 'PHP','Posted_By'=>'ahmed','created_at'=>'2022-10-10 09:00:00' ],
+            ['id' => 2, 'title' => 'Laravel','Posted_By'=>'mohamed','created_at'=>'2022-10-15 09:10:00' ],
+            ['id' => 3, 'title' => 'Vue','Posted_By'=>'fatma','created_at'=>'2022-10-17 09:20:00' ],
+            ['id' => 4, 'title' => 'React','Posted_By'=>'sara','created_at'=>'2022-10-18 09:30:00' ],
+            ['id' => 5, 'title' => 'Node','Posted_By'=>'amr','created_at'=>'2022-10-20 09:40:00' ],
+            ['id' => 6, 'title' => 'Angular','Posted_By'=>'mona','created_at'=>'2022-10-22 09:50:00' ]
+        ];
+
+        return view('posts.index',['posts'=>$allPosts]);
+    }
+}
